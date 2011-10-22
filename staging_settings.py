349b9a9478d80a87b21c444fdd595927bdf934bb
Mishal Awadah <mish@shopwithstella.com>
2011-10-22 17:54:59 -0400

    added staging and production settings files

---
+++ staging_settings.py
@@ -0,0 +1,29 @@
+"""
+These are settings specific to the staging environment. 
+
+ - DEBUG is still on
+ - DB is specific to staging env
+ - CACHE_BACKEND 
+ - EMAIL_HOST and EMAIL_PORT might change once more intricate staging occurs
+ - SITE_ID
+ - The global logging level (via logging.root.setLevel())
+
+"""
+
+from base_settings import *
+
+# The DB should mimic the one we will end up using 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stelladb_staging',
+        'USER': 'postgres',
+        'PASSWORD': '123shockchewy123',
+        'HOST': '',
+        }
+}
+
+TEMPLATE_DIRS = (
+    "/var/www/shopwithstella.com/dev/stella_root/stella_project/templates",
+)
