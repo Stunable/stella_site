d8a880048e894bafacd07c9dac0734ab3d6130ec
damon ciarelli <gdamon@gmail.com>
2012-11-28 11:26:56 -0500

    tweaked settings, removed ssl middleware

--- settings.py
+++ settings.py
@@ -121,7 +121,7 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'sslify.middleware.SSLifyMiddleware',
+    # 'sslify.middleware.SSLifyMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
