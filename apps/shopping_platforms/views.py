237206e3d83c1ed760c73953b748cd04748fa77b
damon <gdamon@gmail.com>
2013-04-21 10:18:00 -0400

    tabs to spaces

--- apps/shopping_platforms/views.py
+++ apps/shopping_platforms/views.py
@@ -16,27 +16,27 @@ from django.contrib.auth import login
 
 
 def get_api(api):
-	# shopify has its own thing...
-	return {
-		'portable':PortableConnection
+    # shopify has its own thing...
+    return {
+        'portable':PortableConnection
 
-	}[api]()
+    }[api]()
 
 def hookup(request,API=None):
 
-	connection,retailer_profile = get_api(API).get_or_create_from_request(request)
+    connection,retailer_profile = get_api(API).get_or_create_from_request(request)
 
-	if connection:
-		request.session['active_api_connection'] = connection
-		request.session['active_retailer_profile'] = retailer_profile
+    if connection:
+        request.session['active_api_connection'] = connection
+        request.session['active_retailer_profile'] = retailer_profile
 
-	if retailer_profile.user:
-		user = retailer_profile.user
+    if retailer_profile.user:
+        user = retailer_profile.user
         user.backend = 'django.contrib.auth.backends.ModelBackend'
         login(request, user)
 
 
 
-		return redirect(reverse('product_list'))
-	else:	
-		return redirect(reverse('create_retailer_profile'))
+        return redirect(reverse('product_list'))
+    else:   
+        return redirect(reverse('create_retailer_profile'))
